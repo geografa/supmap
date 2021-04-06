da484356be38c422c4821b00030d515a5101cf60
Rafa Gutierrez <rafa@mapbox.com>
2021-04-06 10:14:16 -0700

    bmac

--- spots/config.js
+++ spots/config.js
@@ -7,7 +7,7 @@ var config = {
     title: 'Best Portland SUP Spots',
     subtitle: 'A short guide to great paddle places within one hour of Portland, Oregon',
     byline: 'by Rafa Gutierrez',
-    footer: 'Source: Rafa Gutierrez, newb paddler, Portland OR. Opinions my own. Always wear a PFD, leash, and something fabulous. Like the content? Help me <a href="https://www.buymeacoffee.com/supr">buy my son\'s next board!</a>. Copyright © 2021.',
+    footer: 'Source: Rafa Gutierrez, newb paddler, Portland OR. Opinions my own. Always wear a PFD, leash, and something fabulous. Like the content? <a href="https://www.buymeacoffee.com/supr">You can buy me a bevie ☕️ 🍺</a>. Copyright © 2021.',
     chapters: [
         {
             id: 'id-start',
